c1e5d39e1aa51c7aa9667abe9f9af18db58eff14
Belonias <iakovosbelonias@gmail.com>
2017-05-18 23:45:02 +0300

    product cbv and fbv, template in app

--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 
 # Create your views here.
+from .models import Product
+
+# cbv
+class ProductDetailView(DetailView):
+    model = Product
+
+
+def product_detail_view(request, id):
+    # we get the instance of product based on id
+    product_instance = Product.objects.get(id=id)
+    context = {
+        'object': product_instance
+    }
+    template = 'products/product_detail.html'
+    return render(request, template, context)
